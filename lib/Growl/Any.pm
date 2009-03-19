b8fa599b3a8153f5648477ca0b12b55ba73575be
mattn <mattn.jp@gmail.com>
2009-03-19 15:26:21 +0900

    first import

---
+++ lib/Growl/Any.pm
@@ -0,0 +1,33 @@
+package Growl::Any;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Growl::Any -
+
+=head1 SYNOPSIS
+
+  use Growl::Any;
+
+=head1 DESCRIPTION
+
+Growl::Any is
+
+=head1 AUTHOR
+
+Yasuhiro Matsumoto E<lt>mattn.jp@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
