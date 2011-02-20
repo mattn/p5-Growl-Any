cf495af8b47e5cb8c6947e3dc42fe963cece0616
Fuji, Goro <gfuji@cpan.org>
2011-02-20 13:41:42 +0900

    Fix examples/growl.pl

--- examples/growl.pl
+++ examples/growl.pl
@@ -3,7 +3,11 @@ use warnings;
 use utf8;
 use Growl::Any;
 
-my $g = Growl::Any->new;
-$g->register('mattn', ['sleep']);
-$g->notify('sleep', 'mattn as TKSK', 'ZIGOROu の説教まじはんぱねえ', 'http://st-hatena.com/users/to/tokuhirom/profile.gif');
+my $g = Growl::Any->new(
+    appname => 'mattn',
+    events  => ['sleep']
+);
+$g->notify('sleep', 'mattn as TKSK',
+    'ZIGOROu の説教まじはんぱねえ',
+    'http://st-hatena.com/users/to/tokuhirom/profile.gif');
 
